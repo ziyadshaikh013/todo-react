de5c2fdcdec1fac292919f3b894e72aedac94467
ziyadshaikh013 <ziyadshaikh013@gmail.com>
2020-03-20 12:51:59 +0530

    Updates

---
+++ precache-manifest.c3f69802c0ab642a6ebeaf6e3656e58c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f42e1b6a09b74e24f9abbe6bf52dbcf",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "5dcf9cf61ccb171d2366",
+    "url": "/todo-react/static/css/main.1fe4168b.chunk.css"
+  },
+  {
+    "revision": "eb0fc3cfd471a5192149",
+    "url": "/todo-react/static/js/2.08c1aa82.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/todo-react/static/js/2.08c1aa82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dcf9cf61ccb171d2366",
+    "url": "/todo-react/static/js/main.c0271a20.chunk.js"
+  },
+  {
+    "revision": "e5cc6c0f87aede2317e1",
+    "url": "/todo-react/static/js/runtime-main.60e92ffe.js"
+  }
+]);
